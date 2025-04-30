404addef5bde6bbbb395f83f90b6f3376bdd7db0
Faraz Shariq <farazshariqfozzy@gmail.com>
2025-04-30 15:26:29 +0530

    Update CategoryHelper.js

--- mini-incident-management-system/ScriptInclude-GlideAjax/CategoryHelper.js
+++ mini-incident-management-system/ScriptInclude-GlideAjax/CategoryHelper.js
@@ -1,1 +1,22 @@
+var CategoryHelper = Class.create();
+CategoryHelper.prototype = Object.extendsObject(AbstractAjaxProcessor, {
+
+    getDefaultDescription: function() {
+        var category = this.getParameter('sysparm_category');
+        if (!category) return '';
+
+        category = category.toLowerCase().trim(); // Normalize input
+
+        if (category == 'network') {
+            return 'Please check your network connection and try again.';
+        } else if (category == 'software') {
+            return 'The software is experiencing issues.';
+        } else if (category == 'hardware') {
+            return 'Hardware needs to be checked.';
+        } else {
+            return 'General incident description.';
+        }
+    }
+
+});
 
