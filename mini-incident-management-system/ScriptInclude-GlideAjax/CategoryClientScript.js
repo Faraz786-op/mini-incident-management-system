54335c91411ab248eb67f5f6264c780a7ca88e15
Faraz Shariq <farazshariqfozzy@gmail.com>
2025-04-30 15:27:21 +0530

    Update CategoryClientScript.js

--- mini-incident-management-system/ScriptInclude-GlideAjax/CategoryClientScript.js
+++ mini-incident-management-system/ScriptInclude-GlideAjax/CategoryClientScript.js
@@ -1,1 +1,18 @@
+function onChange(control, oldValue, newValue, isLoading) {
+    if (isLoading || newValue == '') {
+        return;
+    }
+    var ga = new GlideAjax('CategoryHelper');
+    ga.addParam('sysparm_name', 'getDefaultDescription');
+    ga.addParam('sysparm_category', newValue);
+
+    ga.getXMLAnswer(function(response) {
+         var answer = response;
+         if (answer) {
+             g_form.setValue('u_description', answer);
+         } else {
+             g_form.setValue('u_description', '');
+        }
+   });
+}
 
